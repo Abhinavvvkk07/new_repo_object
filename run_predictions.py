86066ec2ef5796101a14ea20855e0f5309476bfc
Abhinavvvkk07 <159828780+Abhinavvvkk07@users.noreply.github.com>
2024-10-31 16:33:00 -0400

    Update run_predictions.py
    Script for testing the model

--- run_predictions.py
+++ run_predictions.py
@@ -6,7 +6,7 @@ model = YOLO('best_new.pt')  # Ensure this file is in the same directory
 
 # Run predictions on an image file passed as a command-line argument
 if len(sys.argv) > 1:
-    image_path = sys.argv[1]  # Get the image path from the command line
+    image_path = sys.argv[1]  # Get the image path from the command linee
     results = model.predict(source=image_path, save=True)
     print(f"Results saved in: {results[0].save_dir}")
 else:
